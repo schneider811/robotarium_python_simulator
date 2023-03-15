4299eb14e4075cb4ea1c1b9d777e79e56a6bbcd1
Keith Schneider <Keith.schneider811@gmail.com>
2023-03-15 15:11:47 -0400

    working

--- project_Schneider.py
+++ project_Schneider.py
@@ -10,6 +10,9 @@ from scipy.spatial import Voronoi, voronoi_plot_2d
 from matplotlib.artist import Artist
 import csv
 import pandas as pd
+from copy import deepcopy
+
+
 break_out_flag=False
 
 # c_v is value for grid cell c_v=np.zeros(2,N)
@@ -41,18 +44,19 @@ def get_sensor(j,q):
 
 
 #Scenario 1
-N=10
-sensors=[1,2]
-robot_by_sensor=[1,1,1,1,1,2,2,2,2,2]
-numbots1=5
-numbots2=5
-binary_robot_by_sensor=[[1,1,1,1,1,0,0,0,0,0],[0,0,0,0,0,1,1,1,1,1]]
-n1=[1,2,3,4,5]
-n2=[6,7,8,9,10]
-hi1=[[1,1,1,1,1,0,0,0,0,0],[0,0,0,0,0,1,1,1,1,1]]
-wi1=[[1,1,1,1,1,0,0,0,0,0],[0,0,0,0,0,1,1,1,1,1]]
-initial_conditions = np.asarray([[1.25, 0.25, 0],[1, 0.5, 0],[1, -0.5, 0],[-1, -0.75, 0],[0.1, 0.2, 0],[0.2, -0.6, 0],[-0.75, -0.1, 0],[-1, 0, 0],[-0.8, -0.25, 0],[1.3, -0.4, 0]])
-gain=.2 #.1 gain causes HG to get pretty stable to be ~400
+# N=10
+# sensors=[1,2]
+# robot_by_sensor=[1,1,1,1,1,2,2,2,2,2]
+# numbots1=5
+# numbots2=5
+# binary_robot_by_sensor=[[1,1,1,1,1,0,0,0,0,0],[0,0,0,0,0,1,1,1,1,1]]
+# n1=[1,2,3,4,5]
+# n2=[6,7,8,9,10]
+# hi1=[[1,1,1,1,1,0,0,0,0,0],[0,0,0,0,0,1,1,1,1,1]]
+# wi1=[[1,1,1,1,1,0,0,0,0,0],[0,0,0,0,0,1,1,1,1,1]]
+# initial_conditions = np.asarray([[1.25, 0.25, 0],[1, 0.5, 0],[1, -0.5, 0],[-1, -0.75, 0],[0.1, 0.2, 0],[0.2, -0.6, 0],[-0.75, -0.1, 0],[-1, 0, 0],[-0.8, -0.25, 0],[1.3, -0.4, 0]])
+# gain=.2 #.1 gain causes HG to get pretty stable to be ~400
+
 #scenario 2
 
 # N=10
@@ -86,18 +90,18 @@ gain=.2 #.1 gain causes HG to get pretty stable to be ~400
 
 
 #scenario 4
-# N=5
-# gain=.5
-# sensors=[1,2]
-# robot_by_sensor=[1,1,1,2,2]
-# numbots1=5
-# numbots2=5
-# binary_robot_by_sensor=[[1,1,1,0,0],[0,0,1,1,1]]
-# n1=[1,2,3]
-# n2=[3,2,1]
-# hi1=[[1,1,1,0,0],[0,0,1,1,2]]
-# wi1=[[1,1,1,0,0],[0,0,1,2,2]]
-# initial_conditions = np.asarray([[1.25, 0.25, 0],[1, 0.5, 0],[1, -0.5, 0],[-1, -0.75, 0],[0.1, 0.2, 0]])
+N=5
+gain=.5
+sensors=[1,2]
+robot_by_sensor=[1,1,1,2,2]
+numbots1=5
+numbots2=5
+binary_robot_by_sensor=[[1,1,1,0,0],[0,0,1,1,1]]
+n1=[1,2,3]
+n2=[3,2,1]
+hi1=[[1,1,1,0,0],[0,0,1,1,2]]
+wi1=[[1,1,1,0,0],[0,0,1,2,2]]
+initial_conditions = np.asarray([[1.25, 0.25, 0],[1, 0.5, 0],[1, -0.5, 0],[-1, -0.75, 0],[0.1, 0.2, 0]])
 
 
 # #Scenario 5
@@ -186,7 +190,7 @@ for ii in range(x.shape[1]):
 
 robot_number_labels=[]
 for xx in range(x.shape[1]):
-    robot_number_labels.append(r.axes.text(x[0,0],x[1,xx]+0.25,str(xx),fontsize=font_size, color='r',fontweight='bold',horizontalalignment='center',verticalalignment='center',zorder=0))
+    robot_number_labels.append(r.axes.text(x[0,0],x[1,xx]+0.25,str(int(xx+1)),fontsize=font_size, color='r',fontweight='bold',horizontalalignment='center',verticalalignment='center',zorder=0))
 
 
 #robot_markers = [r.axes.scatter(x[0,ii], x[1,ii], s=marker_size_robot, marker='o', facecolors='none',edgecolors=CM[ii,:],linewidth=line_width) for ii in range(goal_points.shape[1])]
@@ -271,14 +275,20 @@ for iteration in range(iterations):
                     for k in range(len(sensors)):
                         distances3=[0]*N
                         for robot in range(N):
-                                    distances3[robot] = (np.square(xi - x_si[0,robot]) + np.square(yi - x_si[1,robot])) - wi1[k][robot]
+                                    if binary_robot_by_sensor[k][robot] == 1:
+                                        distances3[robot] = (np.square(xi - x_si[0,robot]) + np.square(yi - x_si[1,robot])) - wi1[k][robot]
                                 #    print(distances3)
                         # mask = (distances3[:] > 0)
                         k2=np.argmin(distances3[distances3 != 0])
                         density=get_sensor(1,[x_sample,y_sample])
-                        hg += ((np.square(x_si[0,k2]-xi) + np.square(x_si[1,k2]-yi ))-wi1[k][k2])*density
-
-
+                        #hg += ((np.square(x_si[0,k2]-xi) + np.square(x_si[1,k2]-yi ))-wi1[k][k2])*density
+                        #hg += ((np.square(x_si[0,k2]-(c_v[k2][0]/w[k2])) + np.square(x_si[1,k2]-(c_v[k2][1]/w[k2]) ))-wi1[k][k2])*density
+                        hg += distances3[k2]*density
+#LINES 279 swapped to 280
+#
+#
+#
+#
     #print(hg)
 
 
@@ -302,10 +312,12 @@ for iteration in range(iterations):
     #Equation 14
     sum_of_neighbors_matrix= [[0 for col in range(len(sensors))] for row in range(N)]
 
+    weight_array =[0]*N
+    # #EQ 14 from PAPER 2
+    #updating the weights they do not converge to health
 
-    #EQ 14 from PAPER 2
     for robots in range(N):
-        sum_of_neighbors=0
+        #sum_of_neighbors=0
         z=topological_neighbors(L,robot)
         for k in range(len(sensors)):
             for neighbors in z:
@@ -314,26 +326,34 @@ for iteration in range(iterations):
                 #print(neighbors)
                 sum_of_neighbors_matrix[robots][k]+=hi1[k][robots]-hi1[k][neighbors]
     #print(sum_of_neighbors)
-    sum_of_neighbors_array=np.array(sum_of_neighbors_matrix, dtype=int)         
-    weight_array=gain/(2*density)*sum_of_neighbors_array
-   
+        sum_of_neighbors_array=np.array(sum_of_neighbors_matrix, dtype=int)
+        if not w[robots] == 0:  
+            weight_array[robots]=(gain/(2*w[robots]))*sum_of_neighbors_array[robots]
+    #print("weight array ",weight_array)
+    # weight_array=gain/(2*density)*sum_of_neighbors_array
+    
+
+    #print('wi1',wi1)
     for sensors_num in range(len(sensors)):
         for robot in range(N):
             # if weight_array[robot][sensors_num]<0:
             #     wi1[sensors_num][robot] = 0
             # else:
-                wi1[sensors_num][robot] = weight_array[robot][sensors_num]
+                wi1[sensors_num][robot] = wi1[sensors_num][robot]+ weight_array[robot][sensors_num]
                 wijminushij[sensors_num][robot]=wi1[sensors_num][robot]-hi1[sensors_num][robot]
 
-    values_to_write.append(wi1)
-    values_to_write.append(wijminushij)
+    #normalize weight array
+
+    print('wi1 after changing',wi1)
+    values_to_write.append(deepcopy(wi1))
+    values_to_write.append(deepcopy(wijminushij))
     cwij=[c_v[robot][0]/(w[robot]),c_v[robot][1]/(w[robot])]
     values_to_write.append(cwij)
     #print(values_to_write)
     
     #print(wi1)
     
-    if brake_check:
+    if brake_check:#and convergence of wij-hij:
          print("Simulation has ended due to brake checking movement of robots at iteration: ",iteration)
          break
     # Create safe control inputs (i.e., no collisions)
@@ -344,14 +364,21 @@ for iteration in range(iterations):
     dxu = si_to_uni_dyn(dxi, x)
     for i in range(N):
         distance_traveled[i]+=np.linalg.norm((x_si[:,i])-(previous_x_si[:,i]))
+
+    #Why is this distance traveled working so poorly?
+    #
+    #
+    #
+    #
+
     #print(distance_traveled)
     #old_traveled=distance_traveled
     
 
     #this print debug statement is working as I would hope it would work but writing the distance traveled to values_to_write only is writing the total distance traveled for each
-    print(distance_traveled)
+    # print(distance_traveled)
 
-    values_to_write.append(distance_traveled)
+    values_to_write.append(distance_traveled[:])
 
     total_distance_among_all_bots=np.sum(distance_traveled)
 
@@ -365,15 +392,19 @@ for iteration in range(iterations):
     r.set_velocities(np.arange(N), dxu)
     # Iterate the simulation
     r.step()
+
+
+print(x_si)
+
 firstrow=["Iteration","Pose_x_y_Theta","Wij","Wij_minus_Hij","Cwij","Distance_traveled_by_bot","Total_distance_traveled_by_all","H_Global_locational_Cost"]
 with open('project_schneider.csv','w') as f1:
     writer=csv.writer(f1, delimiter=',',lineterminator='\n',)
     writer.writerow(firstrow)
     writer.writerows(all_values)
 
-df=pd.read_csv('project_schneider.csv')
+# df=pd.read_csv('project_schneider.csv')
 
-df_2 = df.groupby(['Iteration'])
-df_2.plot(y="H:Global locational Cost")
+# df_2 = df.groupby(['Iteration'])
+# df_2.plot(y="H:Global locational Cost")
 
 #r.call_at_scripts_end()    
